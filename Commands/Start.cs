cba402f2a59efc1e71fda2639ebe2c04ab100731
TaggedDev <45800215+TaggedDev@users.noreply.github.com>
2021-05-25 17:48:42 +0300

    Light Start.cs editing

--- Commands/Start.cs
+++ Commands/Start.cs
@@ -8,13 +8,17 @@ namespace TestTaskTelegramBot.Commands
 {
     class Start
     {
+        /// <summary>
+        /// A message that sends to user when he starts the bot
+        /// </summary>
+        /// <param name="message"></param>
         public async static void StartMessage(Message message)
         {
             string messageText = $"Приветствуем вас в ресторане \"Chäf\", {message.Chat.LastName} {message.Chat.FirstName}.";
             var inlineKeyboard = new InlineKeyboardMarkup(new[]
                            {
                                 new[] { InlineKeyboardButton.WithCallbackData("Меню", "start:menu") },
-                                new[] { InlineKeyboardButton.WithCallbackData("Корзина", "start:cart") }
+                                new[] { InlineKeyboardButton.WithCallbackData("Корзина", "cart:overview") }
                             });
             await Bot.Get().SendTextMessageAsync(chatId: message.Chat.Id, text: messageText, replyMarkup: inlineKeyboard);
         }
