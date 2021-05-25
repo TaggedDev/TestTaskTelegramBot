c722a3b98363bbba579e84284bb107bb746a133e
TaggedDev <45800215+TaggedDev@users.noreply.github.com>
2021-05-25 17:47:10 +0300

    Adding to the shopping cart process

--- Commands/Menu.cs
+++ Commands/Menu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Telegram.Bot.Types;
@@ -10,7 +11,7 @@ namespace TestTaskTelegramBot.Commands
     class Menu
     {
         /// <summary>
-        /// Функция для открытия главной страницы меню
+        /// Main restaurant menu message function
         /// </summary>
         public async static void StartMenu(long chatId)
         {
@@ -24,17 +25,19 @@ namespace TestTaskTelegramBot.Commands
         }
 
         /// <summary>
-        /// Функция для отправки блюд 
+        /// Function sends user the list of all the dishes in the category 
         /// </summary>
         public async static void SendDishesCategory(ChatId chatId, string name)
         {
-            List<Dish> dishes = new List<Dish>();
-
+            List<Dish> dishes = GetDishes(name);
+            
             for (int i = 0; i < dishes.Count; i++)
             {
                 var inlineKeyboard = new InlineKeyboardMarkup(new[]
                                {
-                                new[] { InlineKeyboardButton.WithCallbackData("Добавить в корзину", $"menu:add_to_chart{dishes[i].ItemId}") }
+                                new[] { InlineKeyboardButton.WithCallbackData("Добавить в корзину", $"menu:add_to_cart{dishes[i].ItemId}") },
+                                new[] { InlineKeyboardButton.WithCallbackData("Назад", $"start:menu") },
+                                new[] { InlineKeyboardButton.WithCallbackData("Посмотреть корзину", $"cart:overview") }
                             });
 
                 await Bot.Get().SendPhotoAsync(
@@ -46,7 +49,49 @@ namespace TestTaskTelegramBot.Commands
             }
         }
 
-        public async static void AddToCart(long chatId, string dishId)
+        /// <summary>
+        /// Being used to create a list of dishes in one category
+        /// </summary>
+        /// <param name="name">Case-sensitive category name</param>
+        /// <returns>Returns List of Dish and if finds nothing, returns empty list</returns>
+        private static List<Dish> GetDishes(string name)
+        {
+            List<Dish> dishes = new List<Dish>();
+            using (var connection = new SqliteConnection("Data Source=chef.db"))
+            {
+                connection.Open();
+                string sqlExpression = $"SELECT * FROM items";
+                SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+                using (SqliteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows) // If there is data
+                        while (reader.Read())   // Reads data by lines
+                        {
+                            string category = Convert.ToString(reader["category"]);
+
+                            if (category.Equals(name))
+                            {
+                                string itemName = Convert.ToString(reader["name"]),
+                                    description = Convert.ToString(reader["description"]),
+                                           link = Convert.ToString(reader["picture"]),
+                                    cookingTime = Convert.ToString(reader["cooking_time"]);
+                                int  price = Convert.ToInt32(reader["price"]),
+                                    itemId = Convert.ToInt32(reader["item_id"]);
+                                Dish dish = new Dish(itemName, description, link, cookingTime, itemId, price);
+                                dishes.Add(dish);
+                            }       
+                        }
+                }
+            }
+            return dishes;
+        }
+
+        /// <summary>
+        /// Adds dish to the cart by ID
+        /// </summary>
+        /// <param name="chatId">User's chat id</param>
+        /// <param name="dishId">Id of the dish</param>
+        public static void AddToCart(long chatId, string dishId)
         {
             DatabaseHandler.AddItem(chatId, dishId);
         }
--- Service/DatabaseHandler.cs
+++ Service/DatabaseHandler.cs
@@ -1,14 +1,52 @@
 ﻿using Microsoft.Data.Sqlite;
+using System;
 
 namespace TestTaskTelegramBot.Service
 {
     class DatabaseHandler
     {
+        /// <summary>
+        /// Updates user's shopping cart
+        /// </summary>
+        /// <param name="chatId">User's chat id</param>
+        /// <param name="dishId">Id of the dish</param>
         public static void AddItem(long chatId, string dishId)
         {
-            ExecuteSQL("");
+            string userCart = GetCart(chatId);
+            userCart = userCart + ";" + dishId;
+            ExecuteSQL($"UPDATE users SET cart = \'{userCart}\'");
         }
 
+        /// <summary>
+        /// Returns the cart from the users in chef.DB
+        /// </summary>
+        /// <param name="chatId">User's chat id</param>
+        private static string GetCart(long chatId)
+        {
+            using (var connection = new SqliteConnection("Data Source=chef.db"))
+            {
+                connection.Open();
+                string sqlExpression = $"SELECT * FROM users";
+                SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+                using (SqliteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows) // если есть данные
+                        while (reader.Read())   // построчно считываем данные
+                        {
+                            string getId = Convert.ToString(reader[$"{chatId}"]);
+
+                            if (getId.Equals(chatId))
+                                return Convert.ToString(reader["cart"]);
+                        }
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Executes command to the chef.DB
+        /// </summary>
+        /// <param name="cmd">Command to execute</param>
         public static void ExecuteSQL(string cmd)
         {
             using (var connection = new SqliteConnection("Data Source=chef.db"))
@@ -22,7 +60,5 @@ namespace TestTaskTelegramBot.Service
                 command.ExecuteNonQuery();
             }
         }
-
-
     }
 }
