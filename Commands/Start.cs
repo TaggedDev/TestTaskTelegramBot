8fb1dc1a9542fd1a2bdb05588c821ede37b7bb77
TaggedDev <45800215+TaggedDev@users.noreply.github.com>
2021-05-26 11:07:56 +0300

    Fixed bug about adding same person to the database

--- Commands/Start.cs
+++ Commands/Start.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
+using TestTaskTelegramBot.Service;
 
 namespace TestTaskTelegramBot.Commands
 {
@@ -20,6 +21,7 @@ namespace TestTaskTelegramBot.Commands
                                 new[] { InlineKeyboardButton.WithCallbackData("Меню", "start:menu") },
                                 new[] { InlineKeyboardButton.WithCallbackData("Корзина", "cart:overview") }
                             });
+            DatabaseHandler.ExecuteSQL($"INSERT OR IGNORE INTO users VALUES ({message.Chat.Id}, \'\')");
             await Bot.Get().SendTextMessageAsync(chatId: message.Chat.Id, text: messageText, replyMarkup: inlineKeyboard);
         }
     }
