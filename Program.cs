e196ac08879a0332badbaad1aa726537d8b5d569
TaggedDev <45800215+TaggedDev@users.noreply.github.com>
2021-05-26 11:06:55 +0300

    Cart buttons handler

--- Program.cs
+++ Program.cs
@@ -31,9 +31,13 @@ namespace TestTaskTelegramBot
         /// </summary>
         private static void LoadDB()
         {
-            DatabaseHandler.ExecuteSQL("CREATE TABLE IF NOT EXISTS items (item_id INTEGER, name VARCHAR(25), description VARCHAR(255), price INTEGER, picture VARCHAR(255), cooking_time VARCHAR(255), category VARCHAR(30))");
-            DatabaseHandler.ExecuteSQL("CREATE TABLE IF NOT EXISTS users (chat_id INTEGER, cart VARCHAR(255))");
-            //DatabaseHandler.ExecuteSQL("INSERT INTO items VALUES (1, \'плов\', \'пловный плов\', 5000, \'https://avatars.githubusercontent.com/u/45800215?v=4\', \'10 мин\', \'main_course\')");
+            DatabaseHandler.ExecuteSQL("CREATE TABLE IF NOT EXISTS items (item_id INTEGER UNIQUE, name VARCHAR(25), description VARCHAR(255), price INTEGER, picture VARCHAR(255), cooking_time VARCHAR(255), category VARCHAR(30))");
+            DatabaseHandler.ExecuteSQL("CREATE TABLE IF NOT EXISTS users (chat_id INTEGER UNIQUE, cart VARCHAR(255))");
+            /*DatabaseHandler.ExecuteSQL("INSERT INTO items VALUES (1, \'плов\', \'сначала укропу, потом кошачьу жопу\', 5000, \'https://www.gastronom.ru/binfiles/images/20170418/b87bb973.jpg', \'10 мин\', \'main_course\')");
+            DatabaseHandler.ExecuteSQL("INSERT INTO items VALUES (2, \'курица\', \'нет блин корова\', 300, \'https://lapku.ru/images/44508/kuritsa-nesushka.jpg', \'15 мин\', \'main_course\')");
+            DatabaseHandler.ExecuteSQL("INSERT INTO items VALUES (3, \'чекактобыло\', \'я не запомнил название\', 1000, \'https://www.patee.ru/r/x6/14/b8/6d/960m.jpg', \'5 мин\', \'main_course\')");
+            DatabaseHandler.ExecuteSQL("INSERT INTO items VALUES (4, \'мороженное\', \'вкусно\', 600, \'https://media-cdn.tripadvisor.com/media/photo-s/18/7c/da/68/bonmot-ice-cream.jpg', \'3 мин\', \'dessert\')");
+            DatabaseHandler.ExecuteSQL("INSERT INTO items VALUES (5, \'баббл ти\', \'дорого за чай\', 800, \'https://p0.zoon.ru/preview/F3rFsvKojZhcLFwv7Qsrng/659x440x85/0/1/8/52497a1040c088193a8b4583_5257ae6087e01.jpg', \'5 мин\', \'dessert\')");*/
         }
 
         /// <summary>
@@ -75,6 +79,15 @@ namespace TestTaskTelegramBot
                     return;
                 case "cart:overview":
                     Cart.Overview(chatId);
+                    return;
+                case "cart:empty":
+                    Cart.Empty(chatId);
+                    return;
+                case "cart:delete":
+                    Cart.Delete(chatId);
+                    return;
+                case "cart:finish":
+                    Cart.Finish(chatId);
                     return;
             }
         }
