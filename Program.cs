1ecfb1d7b36ccfbe909a994ae6fe24fb3346d142
TaggedDev <45800215+TaggedDev@users.noreply.github.com>
2021-05-28 12:48:46 +0300

    Changed database with different products

--- Program.cs
+++ Program.cs
@@ -34,11 +34,12 @@ namespace TestTaskTelegramBot
             DatabaseHandler.ExecuteSQL("CREATE TABLE IF NOT EXISTS items (item_id INTEGER UNIQUE, name VARCHAR(25), description VARCHAR(255), price INTEGER, picture VARCHAR(255), cooking_time VARCHAR(255), category VARCHAR(30))");
             DatabaseHandler.ExecuteSQL("CREATE TABLE IF NOT EXISTS users (chat_id INTEGER UNIQUE, cart VARCHAR(255))");
             DatabaseHandler.ExecuteSQL("CREATE TABLE IF NOT EXISTS orders (order_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, order_time TIMESTAMP, cart VARCHAR(255))");
-            /*DatabaseHandler.ExecuteSQL("INSERT INTO items VALUES (1, \'плов\', \'сначала укропу, потом кошачьу жопу\', 5000, \'https://www.gastronom.ru/binfiles/images/20170418/b87bb973.jpg', \'10 мин\', \'main_course\')");
-            DatabaseHandler.ExecuteSQL("INSERT INTO items VALUES (2, \'курица\', \'нет блин корова\', 300, \'https://lapku.ru/images/44508/kuritsa-nesushka.jpg', \'15 мин\', \'main_course\')");
-            DatabaseHandler.ExecuteSQL("INSERT INTO items VALUES (3, \'чекактобыло\', \'я не запомнил название\', 1000, \'https://www.patee.ru/r/x6/14/b8/6d/960m.jpg', \'5 мин\', \'main_course\')");
-            DatabaseHandler.ExecuteSQL("INSERT INTO items VALUES (4, \'мороженное\', \'вкусно\', 600, \'https://media-cdn.tripadvisor.com/media/photo-s/18/7c/da/68/bonmot-ice-cream.jpg', \'3 мин\', \'dessert\')");
-            DatabaseHandler.ExecuteSQL("INSERT INTO items VALUES (5, \'баббл ти\', \'дорого за чай\', 800, \'https://p0.zoon.ru/preview/F3rFsvKojZhcLFwv7Qsrng/659x440x85/0/1/8/52497a1040c088193a8b4583_5257ae6087e01.jpg', \'5 мин\', \'dessert\')");*/
+            DatabaseHandler.ExecuteSQL("DELETE FROM items");
+            DatabaseHandler.ExecuteSQL("INSERT INTO items VALUES (1, \'Плов с курицей\', \'Вкуснейший плов в казане. В составе: отварной рис, курица, морковь, сладкий перец, заправка для риса\', 700, \'https://www.gastronom.ru/binfiles/images/20170418/b87bb973.jpg', \'10 мин\', \'main_course\')");
+            DatabaseHandler.ExecuteSQL("INSERT INTO items VALUES (2, \'Курица в соевом соусе\', \'Куриное филе, маринованное в соевом соусе с гарниром на выбор: лапша или картофель\', 560, \'https://img.povar.ru/main/a1/8f/ca/2a/kurica_v_medovo-soevom_souse_s_kunjutom-483793.jpg', \'15 мин\', \'main_course\')");
+            DatabaseHandler.ExecuteSQL("INSERT INTO items VALUES (3, \'Лосось с салатом\', \'Нежное филе лосося на пару с салатом, в составе: салат, помидоры, лимон, огурцы\', 700, \'https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_960_720.jpg', \'5 мин\', \'main_course\')");
+            DatabaseHandler.ExecuteSQL("INSERT INTO items VALUES (4, \'Мороженное\', \'Клубничное, ванильное, шоколадное, цена за шарик\', 100, \'https://media-cdn.tripadvisor.com/media/photo-s/18/7c/da/68/bonmot-ice-cream.jpg', \'3 мин\', \'dessert\')");
+            DatabaseHandler.ExecuteSQL("INSERT INTO items VALUES (5, \'Баббл ти\', \'Необычный холодный напиток со вкусным лакомством на дне\', 300, \'https://p0.zoon.ru/preview/F3rFsvKojZhcLFwv7Qsrng/659x440x85/0/1/8/52497a1040c088193a8b4583_5257ae6087e01.jpg', \'5 мин\', \'dessert\')");
         }
 
         /// <summary>
